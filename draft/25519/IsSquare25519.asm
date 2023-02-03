c4eb0c06bb82bb0ab91d4efb452c8aa3034da742
vmasek <vit.masek@tropicsquare.com>
2023-02-03 18:46:05 +0100

    spect fw: x25519

---
+++ draft/25519/IsSquare25519.asm
@@ -0,0 +1,9 @@
+_IsSquare25519
+; input :
+;   Y2[r12]
+; output
+;   Y2^((p-1)/2) mod p_256 [r22]
+; used registers :
+;   {r0,r12,r22} ... r12 keeps value
+
+;... bunch of MUL25519 to calculate r22 <- Y2^((p-1)/2) ...
