585cbb273d18e0de5c478f04bca5be389c11ad68
vmasek <vit.masek@tropicsquare.com>
2023-05-11 18:02:17 +0200

    spect fw: start eddsa verify

---
+++ src/ecc_math/point_dub_ed25519.s
@@ -0,0 +1,56 @@
+; Point Doubling on curve Ed25519
+; Input:
+;               X    Y    Z    T
+;   Point Q1 = (r7,  r8,  r9,  r10)
+; Output:
+;   Q1 = 2 Q1
+;
+; Expects:
+;   Ed25519 prime in r31
+;
+; Intermediate value registers:
+;   r0-4
+
+;   https://datatracker.ietf.org/doc/rfc8032/
+;   
+;      For point addition, the following method is recommended.  A point
+;      (x,y) is represented in extended homogeneous coordinates (X, Y, Z,
+;      T), with x = X/Z, y = Y/Z, x * y = T/Z.
+;   
+;      The neutral point is (0,1), or equivalently in extended homogeneous
+;      coordinates (0, Z, Z, 0) for any non-zero Z.
+;   
+;                A = X1^2
+;                B = Y1^2
+;                C = 2*Z1^2
+;                H = A+B
+;                E = H-(X1+Y1)^2
+;                G = A-B
+;                F = C+G
+;                X3 = E*F
+;                Y3 = G*H
+;                T3 = E*H
+;                Z3 = F*G
+
+poind_dub_ed25519:
+    MUL25519    r0,  r7,  r7    ; r0 = X1^2     r0 = A
+
+    MUL25519    r1,  r8,  r8    ; r1 = Y1^2     r1 = B
+    MUL25519    r2,  r9,  r9    ; r2 = Z1^2
+    ADDP        r2,  r2,  r2    ; r2 = r2 + r2  r2 = C
+
+    ADDP        r3,  r0,  r1    ; r3 = A+B      r3 = A+B = H
+    ADDP        r4,  r7,  r8    ; r4 = X1 + Y1
+    MUL25519    r4,  r4,  r4    ; r4 = r4 * r4
+    SUBP        r4,  r3,  r4    ; r4 = r3 - r4  r4 = H-(X1+Y1)^2 = E
+
+    SUBP        r0,  r0,  r1    ; r0 = r0 - r1  r0 = A-B = G
+    
+    ADDP        r1,  r2,  r0    ; r1 = r2 + r0  r1 = C+G = F
+
+    MUL25519    r7,  r4,  r1    ; r7 = r4 * r1
+    MUL255119   r10, r0,  r1    ; r10 = r0 * r1
+    MUL255119   r8,  r0,  r3    ; r8 = r0 * r3
+    MUL255119   r9,  r4,  r3    ; r9 = r4 * r3
+
+    RET
---
+++ src/ecc_math/spm_ed25519_short.s
@@ -0,0 +1,46 @@
+; Scalar Point Multiplication on curve Ed25519
+; Uses CSWAP Montgomery Ladder method [https://eprint.iacr.org/2017/293]
+;
+; Imputs:
+;   Point P = (r11, r12, r13, r14)
+;   Scalar k = (r28)
+;
+; Output:
+;   Point Q = (r7,  r8,  r9,  r10)
+;
+; Expects:
+;   Ed25519 prime in r31
+;   Ed25519 parameter d in r6
+;
+; Intermediate value registers:
+;   r0-r4 -> intermediate values for point addition/doubling
+;   r6 -> parameter d
+;   (r7,  r8,  r9,  r10) -> Q0
+;   r30 -> counter
+
+spm_ed25519_short:
+    MOVI r7,  0 ;\
+    MOVI r8,  1 ;|-> (r7,  r8,  r9,  r10) = Q0 = "point at infinity O"
+    MOVI r9,  1 ;|
+    MOVI r10, 0 ;/
+
+    MOVI r30, 256
+spm_ed25519_short:
+    ROL r28, r28
+
+    CSWAP r9,  r12
+    CSWAP r10, r13
+    CSWAP r11, r14
+
+    CALL point_add_p256
+    CALL point_dub_p256
+
+    CSWAP r9,  r12
+    CSWAP r10, r13
+    CSWAP r11, r14
+
+    SUBI r30, r30, 1
+    BRNZ spm_ed25519_short
+
+    RET
+    
