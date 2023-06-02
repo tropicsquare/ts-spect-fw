c1027d53ac8e0e97f29c64aef1be1b39665e8c3b
vmasek <vit.masek@tropicsquare.com>
2023-06-02 13:14:22 +0200

    spect fw: remove obsolete draft

--- _draft/256/IsSquare256.asm
+++
@@ -1,9 +0,0 @@
-_IsSquare256
-; input :
-;   Y2[r12]
-; output
-;   Y2^((p-1)/2) mod p_256 [r22]
-; used registers :
-;   {r0,r12,r22} ... r12 keeps value
-
-;... bunch of MUL256 to calculate r22 <- Y2^((p-1)/2) ...
--- _draft/256/W256PointDub.asm
+++
@@ -1,9 +0,0 @@
-_W256PointDub
-; input :
-;   Q[r8,9,10]
-; output :
-;   2*Q[r14,15,16]
-; used registers
-;   {r0...16, r31}
-
-; ... MUL256, ADDP, SUBP, LD ...
