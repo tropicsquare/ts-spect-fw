b35439e87f7f04aec5cc1111072b86dabe43ab47
vmasek <vit.masek@tropicsquare.com>
2023-06-02 13:00:46 +0200

    spect fw: directory refactoring

--- constants_includes.s
+++
@@ -1,7 +0,0 @@
-.include data/const_rom_leyout.s
-;.include data/data_ram_in_const_leyout.s
-.include src/mem_leyouts/eddsa_verify_mem_leyout.s
-.include spect_ops_constants.s
-
-ca_command .eq 0x0000
-ca_op_status .eq 0x1000
