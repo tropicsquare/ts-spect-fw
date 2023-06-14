8d7f5d0924ab061cb6b6c053fb27a7b4111184d9
vmasek <vit.masek@tropicsquare.com>
2023-06-14 17:16:44 +0200

    spect_fw: add x25519 fw for FIT

---
+++ fit/README.md
@@ -0,0 +1,10 @@
+## FIT CTU - ECC test firmware
+
+x25519 rfc : https://www.rfc-editor.org/rfc/rfc7748
+
+## Inputs:
+
+- `0x0000` : coordinate u, 32-byte string
+- `0x0020` : scalar k, 32-byte string
+- `0x0040` : mask for euclidean scalar blinding, 256-bit unsigned integer
+- `0x0060` : mask for z-coordinate randomization, 256-bit unsigned integer
---
+++ fit/data_ram_in_const_leyout.s
@@ -0,0 +1,15 @@
+ca_ecdsa_p .eq 0x0200
+ca_ecdsa_q .eq 0x0220
+ca_ecdsa_exp_low .eq 0x0240
+ca_ecdsa_b .eq 0x0260
+ca_ecdsa_xG .eq 0x0280
+ca_ecdsa_yG .eq 0x02A0
+ca_eddsa_p .eq 0x02C0
+ca_eddsa_q .eq 0x02E0
+ca_eddsa_8q .eq 0x0300
+ca_eddsa_d .eq 0x0320
+ca_eddsa_xG .eq 0x0340
+ca_eddsa_yG .eq 0x0360
+ca_eddsa_m1 .eq 0x0380
+ca_x25519_a24 .eq 0x03A0
+ca_ffff .eq 0x03C0
