901cd10e7249c648b7b7ec4d6dd6f7e1a6949c51
vmasek <vit.masek@tropicsquare.com>
2023-07-12 03:40:22 -0400

    spect fw: update ecdsa key setup

--- src/ecc_crypto/ecdsa_key_setup.s
+++ src/ecc_crypto/ecdsa_key_setup.s
@@ -1,35 +1,70 @@
 ; ECDSA P-256 Key Setup
 ;
 ; Input:
+;   seed k in r16
 ;
 ; Output:
-;   Public Key  Q(x,y) = (0x1000, 0x1020)
-;   Private Key d = 0x1040
-;
+;   Public Key  A(x,y) = r9, r10
+;   Private Key d = r16
+;   Private Key w = r17
+;   Success = r1 - (ok: 0, fail: 1)
 
 ecdsa_key_setup:
-    LD r28, ca_ecdsa_key
-    ST r28, 0x1040
-
     LD r31, ca_ecdsa_q
-    GRV r27
-    SCB r28, r28, r27
 
-    LD r31, ca_ecdsa_p
-    LD r8, ca_ecdsa_b
+    MOVI    r1,  0
+    RETP    r16, r29, r16
 
-    LD r12, ca_ecdsa_xG
-    LD r13, ca_ecdsa_yG
-    MOVI r14, 1
+.ifdef SPECT_ISA_VERSION_1
+    CMPA    r16, 0
+.endif
 
-    CALL spm_p256
-    MOV r1, r11
-    CALL inv_p256
-    MUL256 r9, r9, r1
-    MUL256 r10, r10, r1
+.ifdef SPECT_ISA_VERSION_2
+    XOR     r1,  r1,  r16
+.endif
 
-    ST r9, 0x1000
-    ST r10, 0x1020
+    BRZ ecdsa_key_setup_fail
 
-    END
+.ifdef SPECT_ISA_VERSION_1
+    MOVI    r17, 0
+.endif
+
+.ifdef SPECT_ISA_VERSION_2
+; possible need of 4 GRV
+    TMAC_IT
+    TMAC_IS r16, 0xA
+
+    MOVI    r2,  0x04
+    MOVI    r30, 17
+ecdsa_key_setup_tmac_padding_loop:
+    ROL8    r2,  r2
+    SUBI    r30, r30, 1
+    BRNZ    ecdsa_key_setup_tmac_padding_loop
+
+    TMAC_UP r2
+    TMAC_RD r17
+.endif
+
+    GRV     r27
+    SCB     r28, r16, r27
+
+    LD      r31, ca_ecdsa_p
+    LD      r8, ca_ecdsa_b
+
+    LD      r12, ca_ecdsa_xG
+    LD      r13, ca_ecdsa_yG
+    MOVI    r14, 1
+
+    CALL    spm_p256
+    MOV     r1, r11
+    CALL    inv_p256
+    MUL256  r9, r9, r1
+    MUL256  r10, r10, r1
     
+    MOVI    r1, 0
+
+    RET
+    
+ecdsa_key_setup_fail:
+    MOVI    r1,  1
+    RET
