b85e5bead26312fb0abf9033444428d58dfe22dc
vmasek <vit.masek@tropicsquare.com>
2023-07-12 03:39:26 -0400

    spect fw: update mem leyouts

--- src/mem_leyouts/mem_leyouts_includes.s
+++ src/mem_leyouts/mem_leyouts_includes.s
@@ -1,6 +1,3 @@
 .include const_rom_leyout.s
 ;.include src/mem_leyouts/data_ram_in_const_leyout.s
-.include eddsa_verify_mem_leyout.s
-
-ca_command .eq 0x0000
-ca_op_status .eq 0x1000
+.include internal_mem_leyout.s
