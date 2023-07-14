a8444dbc2f4ca21abb2afbf1c9f3a7239cf5a266
vmasek <vit.masek@tropicsquare.com>
2023-07-14 08:56:47 -0400

    spect fw: add new ops and constants, decode cfg word

--- gen_spect_ops_constants.py
+++ gen_spect_ops_constants.py
@@ -13,11 +13,11 @@ cfg_file = open("spect_ops_constants.s", 'w')
 for op in cfg:
     cfg_file.write("; " + op["name"] + '\n' )
     cfg_file.write("{} .eq 0x{}\n".format(op["name"]+"_id", format(op["id"], '02X')))
-    if op["input"]:
+    if "input" in op.keys() and op["input"]:
         for input in op["input"]:
-            cfg_file.write("{} .eq 0x{}\n".format(op["name"]+"_input_"+input["name"], format(input["address"], '04X')))
-    if op["output"]:
+            cfg_file.write("{} .eq 0x{}\n".format(op["name"]+"_input_"+input["name"], format(input["address"], 'X')))
+    if "output" in op.keys() and op["output"]:
         for output in op["output"]:
-            cfg_file.write("{} .eq 0x{}\n".format(op["name"]+"_output_"+output["name"], format(output["address"], '04X')))
+            cfg_file.write("{} .eq 0x{}\n".format(op["name"]+"_output_"+output["name"], format(output["address"], 'X')))
 
 cfg_file.close()
--- src/spect_descriptors_constants.s
+++ src/spect_descriptors_constants.s
@@ -1,4 +1,2 @@
-ca_spect_op_id .eq 0x0000
-ca_spect_inout_src .eq 0x0004
-ca_spect_op_data_in_size .eq 0x0008
-ca_op_status .eq 0x1000
+ca_spect_cfg_word .eq 0x0100
+ca_spect_res_word .eq 0x1100
