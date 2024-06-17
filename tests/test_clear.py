a7a94f8e62509199d36a9953e6be313941ab1e08
vmasek <vit.masek@tropicsquare.com>
2024-06-17 15:46:32 +0200

    src,test: fix output size of clear op

--- tests/test_clear.py
+++ tests/test_clear.py
@@ -21,6 +21,19 @@ if __name__ == "__main__":
     tc.start(cmd_file)
     ctx = tc.run_op(cmd_file, "clear", 0x0, 0x1, 0, ops_cfg, test_dir, old_context=ctx)
 
+    SPECT_OP_STATUS, SPECT_OP_DATA_OUT_SIZE = tc.get_res_word(test_dir, "clear")
+
+    if (SPECT_OP_STATUS):
+        print("SPECT_OP_STATUS:", hex(SPECT_OP_STATUS))
+        tc.print_failed()
+        sys.exit(1)
+
+    if (SPECT_OP_DATA_OUT_SIZE != 0):
+        print("SPECT_OP_DATA_OUT_SIZE:", hex(SPECT_OP_DATA_OUT_SIZE))
+        tc.print_failed()
+        sys.exit(1)
+
+
     ctx_dict = tc.parse_context(test_dir, "clear")
 
     ret = 0
