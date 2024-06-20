3fb38c4783706308a47b6700e0bb5b77a637f2ee
vmasek <vit.masek@tropicsquare.com>
2024-06-20 08:54:57 +0200

    src,tb: fix ecc key ops dont return l3 result

--- tests/test_ecc_key_gen_store.py
+++ tests/test_ecc_key_gen_store.py
@@ -2,6 +2,7 @@
 import sys
 import random as rn
 import os
+import itertools
 
 import test_common as tc
 
@@ -13,7 +14,7 @@ ecc_key_origin = {
     "ecc_key_store" : 0x2
 }
 
-def test_process(test_dir, run_id, insrc, outsrc, key_type, op):
+def test_process(test_dir, run_id, insrc, outsrc, key_type, op, full_slot=False):
     cmd_file = tc.get_cmd_file(test_dir)
 
     rng = [rn.randint(0, 2**256-1) for i in range(8)]
@@ -23,7 +24,12 @@ def test_process(test_dir, run_id, insrc, outsrc, key_type, op):
 
     slot = rn.randint(0, 31)
 
-    run_name = f"{op}_{run_id}_{slot}"
+    if full_slot:
+        slot_state = "full_slot"
+    else:
+        slot_state = "empty_slot"
+
+    run_name = f"{op}_{run_id}_{slot}_{slot_state}"
 
     tc.print_run_name(run_name)
 
@@ -51,6 +57,8 @@ def test_process(test_dir, run_id, insrc, outsrc, key_type, op):
     input_word = (key_type << 24) + (slot << 8) + tc.find_in_list(op, ops_cfg)["id"]
 
     tc.write_int32(cmd_file, input_word, (insrc<<12))
+    if full_slot:
+        tc.set_key(cmd_file, 0x1234, ktype=0x4, slot=slot*2, offset=0)
 
     if op == "ecc_key_store":
         tc.write_bytes(cmd_file, k, (insrc<<12) +  0x10)
@@ -59,7 +67,11 @@ def test_process(test_dir, run_id, insrc, outsrc, key_type, op):
 
     SPECT_OP_STATUS, SPECT_OP_DATA_OUT_SIZE = tc.get_res_word(test_dir, run_name)
 
-    if (SPECT_OP_STATUS):
+    if (SPECT_OP_STATUS and not full_slot):
+        print("SPECT_OP_STATUS:", hex(SPECT_OP_STATUS))
+        return 1
+
+    if (SPECT_OP_STATUS == 0 and full_slot):
         print("SPECT_OP_STATUS:", hex(SPECT_OP_STATUS))
         return 1
 
@@ -69,62 +81,67 @@ def test_process(test_dir, run_id, insrc, outsrc, key_type, op):
 
     kmem_data, kmem_slots = tc.parse_key_mem(test_dir, run_name)
 
-    if not kmem_slots[0x4][slot<<1]:
-        print("Private Key Slot is empty.")
-        return 1
+    if not full_slot:
+        if not kmem_slots[0x4][slot<<1]:
+            print("Private Key Slot is empty.")
+            return 1
 
-    if not kmem_slots[0x4][(slot<<1)+1]:
-        print("Public Key Slot is empty.")
-        return 1
+        if not kmem_slots[0x4][(slot<<1)+1]:
+            print("Public Key Slot is empty.")
+            return 1
 
-    priv1 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=0)
-    priv2 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=8)
-    pub1 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=8)
+        priv1 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=0)
+        priv2 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=8)
+        pub1 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=8)
 
-    pub2 = pub2_ref
-    if key_type == tc.P256_ID:
-        pub2 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=16)
+        pub2 = pub2_ref
+        if key_type == tc.P256_ID:
+            pub2 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=16)
 
-    priv3 = priv3_ref
-    if key_type == tc.Ed25519_ID:
-        priv3 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=16)
+        priv3 = priv3_ref
+        if key_type == tc.Ed25519_ID:
+            priv3 = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1), offset=16)
 
-    metadata = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=0)
-    key_type_observed = metadata & 0xFF
-    origin_observed = (metadata >> 8) & 0xFF
+        metadata = tc.get_key(kmem_data, ktype=0x04, slot=(slot<<1)+1, offset=0)
+        key_type_observed = metadata & 0xFF
+        origin_observed = (metadata >> 8) & 0xFF
 
-    #print("Curve:  ", hex(metadata & 0xFF))
-    #print("Origin: ", hex((metadata >> 8) & 0xFF))
+        if (not(
+            priv1 == priv1_ref and
+            priv2 == priv2_ref and
+            priv3 == priv3_ref and
+            pub1 == pub1_ref and
+            pub2 == pub2_ref and
+            key_type_observed == key_type and
+            origin_observed == ecc_key_origin[op])
+        ):
+            return 1
 
-    #print("priv1:    ", hex(priv1))
-    #print("priv1_ref:", hex(priv1_ref))
-    #print()
-    #print("priv2:    ", hex(priv2))
-    #print("priv2_ref:", hex(priv2_ref))
-    #print()
-    #print("pub1:     ", hex(pub1))
-    #print("pub1_ref: ", hex(pub1_ref))
-    #print()
-    #print("pub2:     ", hex(pub2))
-    #print("pub2_ref: ", hex(pub2_ref))
-    #print()
+        #print("Curve:  ", hex(metadata & 0xFF))
+        #print("Origin: ", hex((metadata >> 8) & 0xFF))
+
+        #print("priv1:    ", hex(priv1))
+        #print("priv1_ref:", hex(priv1_ref))
+        #print()
+        #print("priv2:    ", hex(priv2))
+        #print("priv2_ref:", hex(priv2_ref))
+        #print()
+        #print("pub1:     ", hex(pub1))
+        #print("pub1_ref: ", hex(pub1_ref))
+        #print()
+        #print("pub2:     ", hex(pub2))
+        #print("pub2_ref: ", hex(pub2_ref))
+        #print()
 
     l3_result = tc.read_output(test_dir, run_name, (outsrc << 12), 1)
     l3_result &= 0xFF
 
-    if (l3_result != 0xc3):
+    if (l3_result != 0xc3 and not full_slot):
         print("L3 RESULT:", hex(l3_result))
         return 1
 
-    if (not(
-        priv1 == priv1_ref and
-        priv2 == priv2_ref and
-        priv3 == priv3_ref and
-        pub1 == pub1_ref and
-        pub2 == pub2_ref and
-        key_type_observed == key_type and
-        origin_observed == ecc_key_origin[op])
-    ):
+    if (l3_result != 0x3c and full_slot):
+        print("L3 RESULT:", hex(l3_result))
         return 1
 
     return 0
@@ -143,91 +160,30 @@ if __name__ == "__main__":
 
     test_dir = tc.make_test_dir(test_name)
 
-# ===================================================================================
-#   ECC Key Generate
-# ===================================================================================
-    op = "ecc_key_gen"
+    test_vars = [
+        ["ecc_key_gen", "ecc_key_store"],
+        [0x0, 0x4],
+        [tc.Ed25519_ID, tc.P256_ID],
+        [True, False]
+    ]
 
-#   Curve = Ed25519, DATA RAM IN / OUT
-    if (test_process(test_dir, "ed25519_ram", 0x0, 0x1, tc.Ed25519_ID, op)):
-        tc.print_failed()
-        ret |= 1
-    else:
-        tc.print_passed()
+    all_comb = list(itertools.product(*test_vars))
+    fail_flag = 0
 
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
+    src = {0x0: "ram", 0x4: "cpb"}
+    curve_str = {tc.Ed25519_ID: "ed25519", tc.P256_ID: "p256"} 
 
-#   Curve = Ed25519, Command Buffer / Result Buffer
-    if (test_process(test_dir, "ed25519_cpb", 0x4, 0x5, tc.Ed25519_ID, op)):
-        tc.print_failed()
-        ret |= 2
-    else:
-        tc.print_passed()
+    for tst_comb in all_comb:
+        op = tst_comb[0]
+        insrc = tst_comb[1]
+        outsrc = tst_comb[1]+1
+        curve = tst_comb[2]
+        full_slot = tst_comb[3]
 
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
+        if (test_process(test_dir, f"{curve_str[curve]}_{src[insrc]}", insrc, outsrc, curve, op, full_slot)):
+            tc.print_failed()
+            fail_flag = fail_flag | 1
+        else:
+            tc.print_passed()
 
-#   Curve = P256, DATA RAM IN / OUT
-    if (test_process(test_dir, "p256_ram", 0x0, 0x1, tc.P256_ID, op)):
-        tc.print_failed()
-        ret |= 4
-    else:
-        tc.print_passed()
-
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
-
-#   Curve = P 256, Command Buffer / Result Buffer
-    if (test_process(test_dir, "p256_cpb", 0x4, 0x5, tc.P256_ID, op)):
-        tc.print_failed()
-        ret |= 8
-    else:
-        tc.print_passed()
-
-# ===================================================================================
-#   ECC Key Store
-# ===================================================================================
-    op = "ecc_key_store"
-
-#   Curve = Ed25519, DATA RAM IN / OUT
-    if (test_process(test_dir, "ed25519_ram", 0x0, 0x1, tc.Ed25519_ID, op)):
-        tc.print_failed()
-        ret |= 1
-    else:
-        tc.print_passed()
-
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
-
-#   Curve = Ed25519, Command Buffer / Result Buffer
-    if (test_process(test_dir, "ed25519_cpb", 0x4, 0x5, tc.Ed25519_ID, op)):
-        tc.print_failed()
-        ret |= 2
-    else:
-        tc.print_passed()
-
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
-
-#   Curve = P256, DATA RAM IN / OUT
-    if (test_process(test_dir, "p256_ram", 0x0, 0x1, tc.P256_ID, op)):
-        tc.print_failed()
-        ret |= 4
-    else:
-        tc.print_passed()
-
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm {test_dir}/*")
-
-#   Curve = P 256, Command Buffer / Result Buffer
-    if (test_process(test_dir, "p256_cpb", 0x4, 0x5, tc.P256_ID, op)):
-        tc.print_failed()
-        ret |= 8
-    else:
-        tc.print_passed()
-
-    if "TS_SPECT_FW_TEST_DONT_DUMP" in os.environ.keys():
-        os.system(f"rm -r {test_dir}")
-        
-    sys.exit(ret)
+    sys.exit(fail_flag)
