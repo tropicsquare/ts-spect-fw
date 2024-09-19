6b03d071be8c8ea91bd43826fa1b8a7b4f34cac7
vmasek <vit.masek@tropicsquare.com>
2024-09-19 13:50:23 +0200

    src,boot: update boot-fw to version without sha512

--- src/boot_main.s
+++ src/boot_main.s
@@ -12,9 +12,6 @@
 ;
 ; Top source for SPECT Boot Firmware
 ;
-;   - sha512_init
-;   - sha512_update
-;   - sha512_final
 ;   - eddsa_verify
 ;
 ; ==============================================================================
@@ -27,15 +24,6 @@ _start:
     LD      r0, ca_spect_cfg_word
     MOVI    r4, 0xFF
     AND     r0, r0, r4
-
-    CMPI    r0, sha512_update_id
-    BRZ     op_sha512_update
-
-    CMPI    r0, sha512_init_id
-    BRZ     op_sha512_init
-
-    CMPI    r0, sha512_final_id
-    BRZ     op_sha512_final
 
     CMPI    r0, eddsa_verify_id
     BRZ     op_eddsa_verify
@@ -52,13 +40,11 @@ set_res_word:
 
 op_eddsa_verify:
     JMP     eddsa_verify
-    JMP     set_res_word
 
-.include    ops/sha512_ops.s
-.include    ecc_crypto/eddsa_verify.s
+.include    field_math/25519/inv_p25519.s
 .include    ecc_math/ed25519/spm_ed25519_short.s
 .include    ecc_math/ed25519/point_add_ed25519.s
 .include    ecc_math/ed25519/point_dbl_ed25519.s
+.include    ecc_crypto/eddsa_verify.s
 .include    ecc_math/ed25519/point_compress_ed25519.s
 .include    ecc_math/ed25519/point_decompress_ed25519.s
-.include    field_math/25519/inv_p25519.s
