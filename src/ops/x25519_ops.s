271d846c8a94c72bf7465167e73eaf9f0125007f
vmasek <vit.masek@tropicsquare.com>
2026-03-03 11:09:51 +0100

    feat (src): X25519 key generation uses 2 GRVs to generate the X25519 key

--- src/ops/x25519_ops.s
+++ src/ops/x25519_ops.s
@@ -45,7 +45,14 @@ op_x25519_kpair_gen:
     MOVI    r1,  x25519_kpair_gen_id
     ST      r1,  ca_op_link
 
+    ; We call GRV twice in case of the external TRNG provides less the 256 bits
+    ; of entropy. The final generated k is from interval [0, 2**256-2], We omit
+    ; to use simple XOR of r18 and r19 due to possible issues caused in TSV
+    GRV     r18
     GRV     r19
+    LD      r31, ca_ffff        ; r31 <- 2^256 - 1
+    REDP    r19, r19, r18       ; r19 <- r19 * 2^256 + r18 (mod 2^256 - 1)
+
     MOVI    r0,  7
     MOVI    r1,  255
     SBIT    r0,  r0, r1
