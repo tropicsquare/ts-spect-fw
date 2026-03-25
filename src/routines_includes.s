6927e4fa81ac68877a010ca58ae585d21aa73a24
vmasek <vit.masek@tropicsquare.com>
2026-03-25 14:04:31 +0100

    refact(src): removed unused files from routines_includes.s

--- src/routines_includes.s
+++ src/routines_includes.s
@@ -32,19 +32,16 @@
 .include   ecc_math/ed25519/point_decompress_ed25519.s
 .include   ecc_math/ed25519/point_add_ed25519.s
 .include   ecc_math/ed25519/point_dbl_ed25519.s
-;.include   ecc_math/ed25519/spm_ed25519_short.s
 .include   ecc_math/ed25519/spm_ed25519_long.s
 .include   ecc_math/ed25519/spm_ed25519_double.s
 .include   ecc_math/ed25519/spm_ed25519_full_masked.s
 .include   ecc_math/ed25519/point_valid_check_ed25519.s
-;.include   ecc_math/ed25519/point_order_check_ed25519.s
 .include   ecc_math/ed25519/point_check_infinity_ed25519.s
 
 ; .......... P256 ..........
 
 .include   ecc_math/p256/point_add_p256.s
 .include   ecc_math/p256/point_dbl_p256.s
-;.include   ecc_math/p256/spm_p256_short.s
 .include   ecc_math/p256/spm_p256_long.s
 .include   ecc_math/p256/spm_p256_double.s
 .include   ecc_math/p256/spm_p256_full_masked.s
