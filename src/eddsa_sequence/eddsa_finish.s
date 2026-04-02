cff7eaa7694be3c8b7c90a786179ab2edbea1e79
vmasek <vit.masek@tropicsquare.com>
2026-04-02 12:25:16 +0200

    doc(src,comments) Add EdDSA Sequence context description to each sub-op routine file

--- src/eddsa_sequence/eddsa_finish.s
+++ src/eddsa_sequence/eddsa_finish.s
@@ -16,6 +16,18 @@
 ;   2) Verify the computed signature
 ;
 ; ==============================================================================
+;
+; Overall EdDSA sequence context
+;   Public key 'A' --------------> ca_eddsa_sign_internal_A
+;   Private key part 's' --------> r26
+;   Private key part 'prefix' ---> r20
+;   Secure Channel Hash ---------> r16
+;   Secure Channel Nonce --------> r17
+;   Nonce 'r' -------------------> r27
+;   Signature part 'R' ----------> ca_eddsa_sign_internal_R
+;   E = SHA512(R, A, M) ---------> r25
+;
+; ==============================================================================
 
 op_eddsa_finish:
     ; Check and Clear OP Link context
--- src/eddsa_sequence/eddsa_verify_e.s
+++ src/eddsa_sequence/eddsa_verify_e.s
@@ -23,6 +23,18 @@
 ;       verified : pass_val
 ;
 ; ==============================================================================
+;
+; Overall EdDSA sequence context
+;   Public key 'A' --------------> ca_eddsa_sign_internal_A
+;   Private key part 's' --------> r26
+;   Private key part 'prefix' ---> r20
+;   Secure Channel Hash ---------> r16
+;   Secure Channel Nonce --------> r17
+;   Nonce 'r' -------------------> r27
+;   Signature part 'R' ----------> ca_eddsa_sign_internal_R
+;   E = SHA512(R, A, M) ---------> r25
+;
+; ==============================================================================
 eddsa_verify_e_pass:
     MOVI        r30,  pass_val
     RET
